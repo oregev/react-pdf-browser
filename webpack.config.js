c9af9a8ae45bdac01b6df59be5afb739d04a2f4d
or regev <or.regev@abra-it.com>
2022-05-29 20:02:17 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    fallback: {
+      process: require.resolve("process/browser"),
+      zlib: require.resolve("browserify-zlib"),
+      stream: require.resolve("stream-browserify"),
+      util: require.resolve("util"),
+      buffer: require.resolve("buffer"),
+      assert: require.resolve("assert"),
+    },
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+      process: "process/browser",
+    }),
+  ],
+};
